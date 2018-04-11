6d9d4b7096e81cd55b9db4d1f779fdc17e756197
Rebecca Hudson <stupidFont@gmail.com>
2018-04-11 11:52:37 -0500

    Add pages that can add and remove UserApps from a User's homepage.

--- app/controllers/user_apps_controller.rb
+++ app/controllers/user_apps_controller.rb
@@ -1,5 +1,9 @@
 class UserAppsController < ApplicationController
   def add
+        user_id = params[:uid] 
+        app_name = params[:app_name]
+        #UserApp.create(user_id:User.first.id.to_int, link_id: Application.first.id.to_int) 
+	#@app = 
   end
 
   def remove
