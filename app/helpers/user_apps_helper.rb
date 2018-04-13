fbb90a2934d22720b79c5846d78f9fb3fb54dea8
Rebecca Hudson <stupidFont@gmail.com>
2018-04-13 11:56:54 -0500

    Remove print statements.

--- app/helpers/user_apps_helper.rb
+++ app/helpers/user_apps_helper.rb
@@ -4,11 +4,9 @@ module UserAppsHelper
         @user = User.find(session[:user_id])
         
         if params[:app_to_remove] 
-            puts "removing an app"
             @app_id = params[:app_to_remove][:app_to_remove].to_i
         elsif params[:app_to_add]
             @app_id = params[:app_to_add][:app_to_add].to_i
-            puts "adding an app" + @app_id.to_s
         end
         
     end
