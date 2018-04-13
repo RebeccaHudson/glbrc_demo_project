8e86c76af180bb873b2e88bf93e5e57c06cc2f3d
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2018-04-13 13:09:16 -0500

    Remove stray comments and unused assignments.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,15 +1,14 @@
 class HomepageController < ApplicationController
   def show_home
-      @foobar = "I have passed in some data"
       if !logged_in?
           redirect_to login_url
       end
-      @user_shown = session[:user_id].to_s
       @user = User.find(session[:user_id])
       user_apps_to_show = @user.user_apps.order(:sort).all
-      #use: .order(:order_for_user) on the list of apps.
       @apps_to_show = []
-      @user_apps_to_show = []   #returns instances of UserApp rather than Application
+      @user_apps_to_show = []   
+      #returns instances of UserApp rather than Application
+
       user_apps_to_show.each do | one_user_app |
          @user_apps_to_show << one_user_app
          @apps_to_show << Application.find(one_user_app.link_id)
